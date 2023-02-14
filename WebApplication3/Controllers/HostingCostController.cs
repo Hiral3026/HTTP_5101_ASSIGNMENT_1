3f2445e6a2c0c7757fbe8691302fa3255bbea73c
Hiral Patel <hiral@hiralpatel3026@gmail.com>
2023-02-14 11:12:06 -0500

    Add project files.

---
+++ WebApplication3/Controllers/HostingCostController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication3.Controllers
+{
+    public class HostingCostController : ApiController
+    {
+        //Get api/HostingCost/{id}
+        public IHttpActionResult Get(int id)
+        {
+             
+                int fortnights = (id + 13) / 14; // Add 13 to round up instead of down
+                double costPerFortnight = 5.50;
+                double subtotal = fortnights * costPerFortnight;
+                double taxRate = 0.13;
+                double tax = subtotal * taxRate;
+                double total = subtotal + tax;
+
+                string[] result = new string[]
+                {
+                $"{fortnights} fortnights at ${costPerFortnight:F2}/FN = ${subtotal:F2} CAD",
+                $"HST {taxRate:P0} = ${tax:F2} CAD",
+                $"Total = ${total:F2} CAD"
+                };
+
+                return Ok(result);
+            }
+    }
+}
---
+++ WebApplication3/Controllers/NumberMachineController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Razor.Editor;
+
+namespace WebApplication3.Controllers
+{
+    public class NumberMachineController : ApiController
+    {
+        //Get api/NumberMachine/{id}
+        public IHttpActionResult Get(int id)
+        {
+            int addResult = id + 10;
+            int subResult = id - 10;
+            int multiResult = id * 10;
+            int divResult = id / 10;
+
+            return Ok(new { AddResult = addResult, SubResult = subResult, MulResult = multiResult, DivResult = divResult });
+        }
+    }
+}
